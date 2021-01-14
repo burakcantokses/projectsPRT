1399df9d477338941d2eb8ec7634b3f593192a0a
Burakcan TOKSES <75482610+burakcantokses@users.noreply.github.com>
2021-01-14 16:53:37 +0300

    buneymisböyle

---
+++ ders/FinalVize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace delta
+{              
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           float vize,final,ortalama;
+           System.Console.WriteLine("Vize Notunuzu Girin.");
+           vize = Convert.ToSingle(Console.ReadLine())/100*40;
+           System.Console.WriteLine("Final Notunuzu Girin.");
+           final = Convert.ToSingle(Console.ReadLine())/100*60;
+           ortalama = (vize+final)/2;
+            System.Console.WriteLine("Ortalamanız: " + ortalama);
+        }
+    }
+}
